9332c5f6448f3847e6039f94ef3fd6b01bb8b066
yachty66 <maxhager28@gmail.com>
2024-03-28 20:15:31 +0100

    init

---
+++ DailyNotes/DailyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyNotesApp.swift
+//  DailyNotes
+//
+//  Created by Max Hager on 28.03.24.
+//
+
+import SwiftUI
+
+@main
+struct DailyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
