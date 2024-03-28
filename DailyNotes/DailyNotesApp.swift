da2352c4f3061c99b80f2c665eeb9dd4dee5fc18
yachty66 <maxhager28@gmail.com>
2024-03-28 21:40:12 +0100

    cleanup

--- DailyNotes/DailyNotesApp.swift
+++ DailyNotes/DailyNotesApp.swift
@@ -2,20 +2,9 @@ import SwiftUI
 
 @main
 struct DailyNotesApp: App {
-    // Initialize the application
     init() {
-        // Check if application was running before
-        // if isFirstLaunch() {
-        //     showPermissionsInstructions()
-        // }
-
-        //requestAutomationPermissions()
-
-
-        // Call the method to check and create the folder
-        CreateFolder.checkAndCreateFolder()
+        checkAndCreateFolder()
     }
-
     var body: some Scene {
         WindowGroup {
             ContentView()
